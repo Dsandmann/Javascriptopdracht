6135bbdda2f87788620b3a09bf01282f5cedda1c
Dennis Sandmann <dennissandmann@Denniss-Air.fritz.box>
2018-01-09 10:13:15 +0100

    first commit

---
+++ opdracht1.js
@@ -0,0 +1,16 @@
+function FindArraysElements(){
+var arrays = [[4,2,0], [8,11,9], [7,0,7], [7,4,28], [3,10,26]];
+
+for (var i = 0; i <= arrays.length - 1; i++){
+document.write("<br/>");
+document.write("Row :" + i + " " + " " + " " +); {
+
+for (var j = 0; j <= arrays[i].lenght-1; j++){
+
+document.write(arrays[i][j]+" " +" ");
+
+}
+}
+}
+
+FindArraysElements();
